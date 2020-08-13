d3289b972b0f7455beeb8aa8066a68e656c22050
Ganna-Pro <prokofievaganna@gmail.com>
2020-08-13 08:25:55 -0600

    Add project files.

---
+++ Bike/BikeClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeApplication
+{
+  public class Bike
+    {         
+        public int speed;
+        public int gear;
+
+
+       public Bike()
+        {
+            speed = 5;
+            gear = 1;
+        }
+
+        public Bike(int bikeSpeed, int bikeGear)
+        {
+            speed = bikeSpeed;
+            gear = bikeGear;
+        }
+        
+        public void Accelerate()
+        {
+            speed++;
+        }
+
+        public void SetGear(int gear)
+         {
+            if (!(gear > 6))
+            this.gear = gear;
+        }
+
+        private void Brakes()
+        {
+            speed--;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Speed: " + speed + "\n Gear:" + gear);
+        }
+    }
+}
+
+
---
+++ Bike/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeApplication
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bike myBike = new Bike();
+            myBike.speed = 30;
+            myBike.gear = 2;
+            myBike.Accelerate();
+            myBike.SetGear(3);
+            myBike.display();
+
+            Bike yourBike = new Bike (40,4);
+            yourBike.speed = 20;
+            yourBike.gear = 1;
+            yourBike.Accelerate();
+            yourBike.SetGear(2);
+            yourBike.display();
+        }
+    }
+}
+    
